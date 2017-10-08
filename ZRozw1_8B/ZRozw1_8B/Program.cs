b1457b33815b1d452ef78e85cce687bbe6f8bf7c
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-08 17:43:04 +0200

    Add files via upload

---
+++ ZRozw1_8B/ZRozw1_8B/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZRozw1_8B
+{
+    class Program
+    {
+        static void PokazSume(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 5, b = -7;
+            PokazSume(a, b);
+            Console.ReadKey();
+        }
+    }
+}
